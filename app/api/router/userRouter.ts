63d87bb83b7adc7716eb3608189b3329bc09fd6c
ridwanullah <wetwan080@gmail.com>
2025-09-23 17:49:23 +0100

    first commit

--- app/api/router/userRouter.ts
+++
@@ -1,9 +0,0 @@
-import { registerUser } from '@/app/controller/user';
-import express from 'express'
-
-const userRoute = express.Router();
-
-
-userRoute.post("/signup", registerUser);
-
-export default userRoute;
