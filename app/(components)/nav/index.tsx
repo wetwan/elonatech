6a8f54b578fab7878a5574e68dc430d037653721
ridwanullah <wetwan080@gmail.com>
2025-09-24 07:43:07 +0100

    hidding the logout button there is no user

--- app/(components)/nav/index.tsx
+++ app/(components)/nav/index.tsx
@@ -4,19 +4,25 @@ import React from "react";
 
 const Nav = () => {
   const { handleLogout } = useTaskCreation();
+  const token = localStorage.getItem("token");
+
   return (
     <div className="flex justify-between p-4  mx-auto items-center my-5">
       <div className="">
         <h1 className="my-5 font-bold text-2xl capitalize">Elonatech</h1>
-        <p className="hidden sm:block font-semibold">Welcome to Elonatech Nigeria Limited</p>
+        <p className="hidden sm:block font-semibold">
+          Welcome to Elonatech Nigeria Limited
+        </p>
       </div>
 
-      <div
-        className="py-5 capitalize px-10 rounded-2xl bg-red-500"
-        onClick={handleLogout}
-      >
-        log out
-      </div>
+      {token ? (
+        <div
+          className="py-5 capitalize px-10 rounded-2xl bg-red-500"
+          onClick={handleLogout}
+        >
+          log out
+        </div>
+      ) : null}
     </div>
   );
 };
